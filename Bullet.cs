ba846114665f96d7e5a0a8870034959e86b7d081
ashkankovsary <ashkankovsaryyy@gmail.com>
2026-07-07 12:41:52 +0330

    add Bullet, Enemy base class, and StandardEnemy

---
+++ Bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+
+namespace Space_Shooter_game
+{
+    public enum BulletOwner
+    {
+        Player,
+        Enemy
+    }
+
+    public class Bullet : GameEntity
+    {
+        public float DirX;
+        public float DirY;
+        public BulletOwner Owner;
+        public int Damage;
+
+        public Bullet(float x, float y, float dirX, float dirY, float speed, BulletOwner owner, int damage)
+            : base(x, y, speed, collisionRadius: 3f)
+        {
+            DirX = dirX;
+            DirY = dirY;
+            Owner = owner;
+            Damage = damage;
+        }
+
+        public override void Move()
+        {
+            X += DirX * Speed;
+            Y += DirY * Speed;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            Color color = Owner == BulletOwner.Player ? Color.Blue : Color.OrangeRed;
+            using (SolidBrush brush = new SolidBrush(color))
+            {
+                g.FillEllipse(brush, X - CollisionRadius, Y - CollisionRadius, CollisionRadius * 2, CollisionRadius * 2);
+            }
+        }
+    }
+}
---
+++ StandardEnemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+namespace Space_Shooter_game
+{
+    public class StandardEnemy : Enemy
+    {
+        public StandardEnemy(float x, float y)
+            : base(x, y, speed: 2f, collisionRadius: 10f, maxHP: 10, scoreValue: 10)
+        {
+        }
+
+        public override void Shoot()
+        {
+        }
+
+        public override void Draw(Graphics g)
+        {
+            using (SolidBrush brush = new SolidBrush(Color.Red))
+            {
+                g.FillEllipse(brush, X - CollisionRadius, Y - CollisionRadius, CollisionRadius * 2, CollisionRadius * 2);
+            }
+        }
+    }
+}
