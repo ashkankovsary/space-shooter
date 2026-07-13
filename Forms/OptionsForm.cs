baebd5f3d33c0b3506aece4dd3b8f13d67fdfe84
Ashkan <ashkanehsani04@gmail.com>
2026-07-13 11:12:56 +0330

    Add audio controls panel

--- Forms/OptionsForm.cs
+++ Forms/OptionsForm.cs
@@ -26,10 +26,49 @@ namespace Space_Shooter_game.Forms
             AudioManager.PlaySfx(Sounds.ClickButton);
             guide_panel.Visible = true;
         }
+        private void audio_btn_Click(object sendr, EventArgs e)
+        {
+            AudioManager.PlaySfx(Sounds.ClickButton);
+            audio_panel.Visible = true;
+        }
         private void back_guide_btn_Click(object sender, EventArgs e)
         {
             AudioManager.PlaySfx(Sounds.ClickButton);
             guide_panel.Visible = false;
+        }
+
+        private void audio_back_btn_Click(object sender, EventArgs e)
+        {
+            AudioManager.PlaySfx(Sounds.ClickButton);
+            audio_panel.Visible = false;
+        }
+
+        private void music_on_Click(object sender, EventArgs e)
+        {
+            AudioManager.PlaySfx(Sounds.ClickButton);
+            music_on.Visible = false;
+            music_off.Visible = true;
+        }
+
+        private void sfx_on_Click(object sender, EventArgs e)
+        {
+            AudioManager.PlaySfx(Sounds.ClickButton);
+            sfx_on.Visible = false;
+            sfx_off.Visible = true;
+        }
+
+        private void music_off_Click(object sender, EventArgs e)
+        {
+            AudioManager.PlaySfx(Sounds.ClickButton);
+            music_on.Visible = true;
+            music_off.Visible = false;
+        }
+
+        private void sfx_off_Click(object sender, EventArgs e)
+        {
+            AudioManager.PlaySfx(Sounds.ClickButton);
+            sfx_on.Visible = true;
+            sfx_off.Visible = false;
         }
     }
 }
