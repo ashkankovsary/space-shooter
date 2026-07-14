bbc32159095089b7133a65063e997973a41296f6
Ashkan <ashkanehsani04@gmail.com>
2026-07-14 18:27:20 +0330

    Add database primery logic

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Space_Shooter_game.Config;
+
 namespace Space_Shooter_game
 {
     internal static class Program
@@ -11,6 +13,7 @@ namespace Space_Shooter_game
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            Database.Initialize();
             Application.Run(new Form1());
         }
     }
